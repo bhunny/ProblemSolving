47f03b175d8f994c8a85357fc7bb49cc2809ee04
lim7217 <lim7217@naver.com>
2022-04-20 17:07:54 +0900

    refactor 로또의최고순위와최저순위 if문을 위에 function으로 만들어 간결하게 수정

--- programmers/로또의최고순위와최저순위.cpp
+++ programmers/로또의최고순위와최저순위.cpp
@@ -1,11 +1,17 @@
 #include <string>
 #include <vector>
-//#include <iostream>
 using namespace std;
 bool chk[46];
 int cnt_zero;
 int cnt_same;
 int result[2];
+int func(int n)
+{
+    if (n < 2)
+        return 6;
+    else
+        return (7 - n);
+}
 vector<int> solution(vector<int> lottos, vector<int> win_nums)
 {
     vector<int> answer;
@@ -18,7 +24,6 @@ vector<int> solution(vector<int> lottos, vector<int> win_nums)
         }
         chk[num] = 1;
     }
-    //cout << cnt_zero;
     for (auto num : win_nums)
     {
         if (chk[num])
@@ -26,35 +31,11 @@ vector<int> solution(vector<int> lottos, vector<int> win_nums)
             cnt_same++;
         }
     }
-    //cout << cnt_same;
     result[0] = cnt_same + cnt_zero;
     result[1] = cnt_same;
     for (auto num : result)
     {
-        if (num == 6)
-        {
-            answer.push_back(1);
-        }
-        else if (num == 5)
-        {
-            answer.push_back(2);
-        }
-        else if (num == 4)
-        {
-            answer.push_back(3);
-        }
-        else if (num == 3)
-        {
-            answer.push_back(4);
-        }
-        else if (num == 2)
-        {
-            answer.push_back(5);
-        }
-        else
-        {
-            answer.push_back(6);
-        }
+        answer.push_back(func(num));
     }
     return answer;
 }

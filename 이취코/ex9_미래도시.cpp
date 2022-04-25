b6b1be592bd7ca6ef2036a6dffa06215c9502647
lim7217 <lim7217@naver.com>
2022-04-25 16:55:18 +0900

    remind ex9 미래도시 플로이드워셜

--- 이취코/ex9_미래도시.cpp
+++ 이취코/ex9_미래도시.cpp
@@ -1,50 +1,44 @@
 #include <bits/stdc++.h>
-#define INF 1e9 // 무한을 의미하는 값으로 10억을 설정
-
 using namespace std;
-
-// 노드의 개수(N), 간선의 개수(M)
+const int INF = 1e9;
 int n, m;
-// 2차원 배열(그래프 표현)를 만들기
 int graph[101][101];
-
-int main(void)
+int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n >> m;
-
-    // 최단 거리 테이블을 모두 무한으로 초기화
     for (int i = 0; i < 101; i++)
     {
-        fill(graph[i], graph[i] + 101, INF);
+        fill(graph[i], graph[i] + 101, INF); // graph의 모든 값을 INF로 초기화
     }
-
-    // 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+    // 자기 자신에게 가는 거리는 0으로 초기화
+    // a 출발, b 도착
     for (int a = 1; a <= n; a++)
     {
         for (int b = 1; b <= n; b++)
         {
             if (a == b)
+            {
                 graph[a][b] = 0;
+            }
         }
     }
-
-    // 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+    // 간선에 대한 정보를 입력
+    // 각 간선 사이의 거리는 1이므로 1로 초기화
     for (int i = 0; i < m; i++)
     {
-        // A와 B가 서로에게 가는 비용은 1이라고 설정
         int a, b;
         cin >> a >> b;
-        graph[a][b] = 1;
-        graph[b][a] = 1;
+        graph[a][b] = 1; // a->b 시간 1
+        graph[b][a] = 1; // b->a 시간 1
     }
-
-    // 거쳐 갈 노드 X와 최종 목적지 노드 K를 입력받기
     int x, k;
     cin >> x >> k;
-
-    // 점화식에 따라 플로이드 워셜 알고리즘을 수행
+    // 1. 우선 a->b로 가는 최단거리를 구한다. d[a][b] = min(d[a][b],d[a][k]+d[k][d])
+    // 2. 그 뒤 k를 바꾸면서 다시 최단거리를 최신화 한다. -> 제일 바깥 for문
     for (int k = 1; k <= n; k++)
-    {
+    { //1~n까지
         for (int a = 1; a <= n; a++)
         {
             for (int b = 1; b <= n; b++)
@@ -53,18 +47,38 @@ int main(void)
             }
         }
     }
+    int dist = graph[1][k] + graph[k][x];
 
-    // 수행된 결과를 출력
-    int distance = graph[1][k] + graph[k][x];
-
-    // 도달할 수 없는 경우, -1을 출력
-    if (distance >= INF)
+    if (dist >= INF)
     {
-        cout << "-1" << '\n';
+        cout << "-1";
     }
-    // 도달할 수 있다면, 최단 거리를 출력
     else
     {
-        cout << distance << '\n';
+        cout << dist;
     }
 }
+/*
+
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+---
+3
+
+
+4 2
+1 3
+2 4
+3 4
+---
+-1
+
+
+*/

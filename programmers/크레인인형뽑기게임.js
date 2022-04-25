cb08d7af86f1f5ad9d434013351c5225c75168da
lim7217 <lim7217@naver.com>
2022-04-25 22:30:45 +0900

    refactor 좀더간결하게 수정

--- programmers/크레인인형뽑기게임.js
+++ programmers/크레인인형뽑기게임.js
@@ -9,22 +9,22 @@ function solution(board, moves) {
     //i(선택할위치)를 선택한 뒤, 보드 배열을 탐색한다.
     for(i of moves){
         for(j of board){
+            //해당위치에 값이 존재하지 않으면 넘어감
+            if(!j[i-1]) continue;
             //바스켓의 제일 마지막 원소와 비교해서 같으면 마지막 원소 빼내고
             //해당 위치 0으로 초기화
             //answer +2
             if(basket && basket[basket.length-1]===j[i-1]){
                 basket.pop();
-                j[i-1] = 0;
                 answer+=2;
-                break;
             }
             //해당 위치에 원소가 존재하면 바스켓에 넣고 해당 위치 0
             else if(j[i-1]) {
                 basket.push(j[i-1]);
-                j[i-1] = 0;
-                break;
+                
             }
-            
+            j[i-1] = 0;
+            break;
         }
     }
     return answer;

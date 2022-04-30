9292488267f01b207d86905ccf62512227e23968
lim7217 <lim7217@naver.com>
2022-04-30 20:09:49 +0900

    refactor 코드 간결하게 수정

--- programmers/타겟넘버.js
+++ programmers/타겟넘버.js
@@ -1,20 +1,17 @@
-var cnt = 0;
-function dfs(arr, depth, target, result){
-    if(depth===arr.length)
-    {
-        if(target ===result){
-            cnt++;
-        }
-            return;
-    }
-    var plus = result + arr[depth];
-    var minus = result - arr[depth];
-    dfs(arr, depth+1, target, plus);
-    dfs(arr, depth+1, target, minus);
-}
 function solution(numbers, target) {
     var answer = 0;
-
-    dfs(numbers, 0, target, 0);
-    return cnt;
+    
+    dfs(0, 0);
+    function dfs(depth, result){
+    if(depth===numbers.length)
+    {
+        if(target ===result){
+            answer++;
+        }
+        return;
+    }
+    dfs(depth+1, result + numbers[depth]);
+    dfs(depth+1, result - numbers[depth]);
+}
+    return answer;
 }
